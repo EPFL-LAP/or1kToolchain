f22b38e8809242e78b28da7163435f12de925951
Theo Kluter <ktt1@bfh.ch>
2025-09-03 08:57:31 +0200

    Updated for latest tool-chain

--- patch/compile_linux.sh
+++ patch/compile_linux.sh
@@ -2,9 +2,9 @@
 mkdir sandbox
 cd sandbox
 rm -rf *
-tar -xvf ../../gnu_src/binutils-2.44.tar.bz2
-patch -u binutils-2.44/cpu/or1korbis.cpu -i ../or1korbis.patch
-cd binutils-2.44/
+tar -xvf ../../gnu_src/binutils-2.45.tar.bz2
+patch -u binutils-2.45/cpu/or1korbis.cpu -i ../or1korbis.patch
+cd binutils-2.45/
 rsync -av ../../../gnu_src/cgen .
 ./configure -target=or1k-elf --disable-werror --prefix=/opt/or1k_toolchain
 make -j$(nproc) all
@@ -14,11 +14,11 @@ cd ..
 make -j$(nproc) all
 make install
 cd ..
-tar -xvf ../../gnu_src/gcc-15.1.0.tar.gz
-patch -u gcc-15.1.0/gcc/config/or1k/elf.h -i ../gcc.patch
+tar -xvf ../../gnu_src/gcc-15.2.0.tar.gz
+patch -u gcc-15.2.0/gcc/config/or1k/elf.h -i ../gcc.patch
 mkdir gcc
 cd gcc/
-../gcc-15.1.0/configure --target=or1k-elf  --prefix=/opt/or1k_toolchain --with-gnu-as --with-gnu-ld --verbose --enable-languages=c --disable-libssp --disable-shared --disable-werror
+../gcc-15.2.0/configure --target=or1k-elf  --prefix=/opt/or1k_toolchain --with-gnu-as --with-gnu-ld --verbose --enable-languages=c --disable-libssp --disable-shared --disable-werror
 make -j$(nproc) all
 make install
 
--- patch/compile_mac.sh
+++ patch/compile_mac.sh
@@ -2,8 +2,8 @@
 mkdir sandbox
 cd sandbox
 rm -rf *
-tar -xvf ../../gnu_src/binutils-2.44.tar.bz2
-cd binutils-2.44/
+tar -xvf ../../gnu_src/binutils-2.45.tar.bz2
+cd binutils-2.45/
 rsync -av ../../../gnu_src/cgen .
 ./configure -target=or1k-elf --disable-werror --prefix=/opt/or1k_toolchain
 make -j$(sysctl -n hw.ncpu) all
@@ -17,11 +17,11 @@ cd ..
 make -j$(sysctl -n hw.ncpu) all
 make install
 cd ..
-tar -xvf ../../gnu_src/gcc-15.1.0.tar.gz
-patch -u gcc-15.1.0/gcc/config/or1k/elf.h -i ../gcc.patch
+tar -xvf ../../gnu_src/gcc-15.2.0.tar.gz
+patch -u gcc-15.2.0/gcc/config/or1k/elf.h -i ../gcc.patch
 mkdir gcc
 cd gcc/
-../gcc-15.1.0/configure --target=or1k-elf  --prefix=/opt/or1k_toolchain --with-gnu-as --with-gnu-ld --verbose --enable-languages=c --disable-libssp --disable-shared --disable-werror --with-gmp=/opt/homebrew/ --with-mpfr=/opt/homebrew/ --with-mpc=/opt/homebrew/
+../gcc-15.2.0/configure --target=or1k-elf  --prefix=/opt/or1k_toolchain --with-gnu-as --with-gnu-ld --verbose --enable-languages=c --disable-libssp --disable-shared --disable-werror --with-gmp=/opt/homebrew/ --with-mpfr=/opt/homebrew/ --with-mpc=/opt/homebrew/
 make -j$(sysctl -n hw.ncpu) all
 make install
 
